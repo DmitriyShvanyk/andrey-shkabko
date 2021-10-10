7700d21991500a6d582528b0047d696cb1f37657
DmitriyShvanyk <soledar1984@gmail.com>
2021-10-10 19:48:14 +0300

    Added page congratulation

---
+++ congrat/assets/index.php
@@ -0,0 +1,467 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<title>ПОЗДРАВЛЯЕМ! </title>
+	<meta charset="utf-8">
+	<meta name="keywords" content='ПОЗДРАВЛЯЕМ! '>
+	<meta name="description" content='ПОЗДРАВЛЯЕМ! '>
+	<meta name="format-detection" content="telephone=no">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<meta property="og:url" content="">
+	<meta property="og:image" content="">
+
+	<!-- favicon -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- preload style -->
+	<link rel="preload" href="css/style.min.css?v=1" as="style">
+
+	<!-- main style -->
+	<link href="css/style.min.css?v=1" rel="stylesheet">
+</head>
+
+<body>
+
+	<!--root.begin-->
+	<div class="root">
+
+		<!--header.begin-->
+		<header class="header">
+			<div class="container header__container">
+
+				<div class="header__content">
+					<h1 class="header__title">
+						ПОЗДРАВЛЯЕМ! 
+					</h1>
+					<div class="header__event">
+						Вы попали в списки участников онлайн-марафона по снижению веса по системе Татьяны Малаховой
+					</div>
+					<a data-fancybox data-src="#hidden-content" href="javascript:;" class="btn btn--active header__btn">
+						<span class="btn__inner">ЗАБРОНИРОВАТЬ МЕСТО</span>						
+					</a>
+				</div>
+
+			</div>
+		</header>
+		<!--header.end-->
+
+		<!--content.begin-->
+		<main class="main">
+
+			<!--section 1-->
+			<section class="s1">
+				<div class="container s1__container">
+					<h2 class="title s1__title">
+						ЧТО ВЫ ПОЛУЧИТЕ
+					</h2>
+					<div class="s1__items">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="s1__item">
+									<img class="s1__img lazyload" loading="lazy" src="#" data-src="img/s1/01.png" data-srcset="img/s1/01@2x.png 2x" alt="">
+									<div class="s1__text">
+										7-дневная безопасная программа снижения веса, разработанная по системе Татьяны Малаховой
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="s1__item">
+									<img class="s1__img lazyload" loading="lazy" src="#" data-src="img/s1/02.png" data-srcset="img/s1/02@2x.png 2x" alt="">
+									<div class="s1__text">
+										Получите первые результаты сразу после марафона и поймёте, что худеть можно легко и просто, не ограничивая себя в еде и калориях
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="s1__item">
+									<img class="s1__img lazyload" loading="lazy" src="#" data-src="img/s1/03.png" data-srcset="img/s1/03@2x.png 2x" alt="">
+									<div class="s1__text">
+										Бесплатная предварительная программа детокса в качестве подготовки к марафону в течение 3&nbsp;дней
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="s1__item">
+									<img class="s1__img lazyload" loading="lazy" src="#" data-src="img/s1/04.png" data-srcset="img/s1/04@2x.png 2x" alt="">
+									<div class="s1__text">
+										«Практическое руководство по расчёту лишнего веса в вашем организме»
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="btn-container">
+						<a data-fancybox data-src="#hidden-content" href="javascript:;" class="btn btn--active s1__btn">
+							<span class="btn__inner">ДА, Я ХОЧУ ПРИНЯТЬ&nbsp;УЧАСТИЕ</span>						
+						</a>
+					</div>					
+				</div>
+			</section>
+			<!--section 1-->
+
+			<!--section 2-->
+			<section class="s2">
+				<div class="container s2__container">
+					<h2 class="title s2__title">
+						Бессрочный доступ к&nbsp;марафону
+					</h2>					
+					<div class="blocks s2__blocks">
+						<div class="blocks__inner">
+							<div class="row">
+								<div class="col-md-4">
+									<div class="block">
+										<img class="block__icon block__icon--1 lazyload" loading="lazy" src="#" data-src="img/s2/i1.svg" alt="">
+										<div class="block__caption">
+											все уроки марафона доступны на любом устройстве в любое время
+										</div>
+									</div>									
+								</div>
+								<div class="col-md-4">
+									<div class="block">
+										<img class="block__icon lazyload" loading="lazy" src="#" data-src="img/s2/i2.svg" alt="">
+										<div class="block__caption">
+											Полезные дополнительные материалы
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="block">
+										<img class="block__icon lazyload" loading="lazy" src="#" data-src="img/s2/i2.svg" alt="">
+										<div class="block__caption">
+											Удобная онлайн-платформа
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</section>
+			<!--section 2-->
+
+			<!--section 3-->
+			<section class="s3">
+				<div class="container s3__container">
+					<h2 class="title s3__title">
+						В результате марафона вы:
+					</h2>
+
+					<div class="s3__items">
+						<div class="row">
+							<div class="col-md-3">
+								<div class="s3__item">
+									<div class="s3__num">1</div>
+									<div class="s3__text">
+										Избавитесь от лишних килограммов с помощью правильного сочетания простых продуктов
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="s3__item">
+									<div class="s3__num">2</div>
+									<div class="s3__text">
+										Разнообразите рацион множеством кулинарных решений
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="s3__item">
+									<div class="s3__num">3</div>
+									<div class="s3__text">
+										Поправите здоровье без помощи врачей и таблеток
+									</div>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="s3__item">
+									<div class="s3__num">4</div>
+									<div class="s3__text">
+										Начнёте новую жизнь, полную энергии и сил
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</section>
+			<!--section 3-->
+
+			<!--section 4-->
+			<section class="s4">
+				<div class="container s4__container">
+
+					<div class="s4__row">
+						<div class="s4__box">
+							<div class="s4__pict">
+								<img class="s4__img lazyload" loading="lazy" src="#" data-src="img/s4/pict1.png" data-srcset="img/s4/pict1@2x.png 2x" alt="">
+							</div>
+							<div class="s4__content">
+								<h3 class="s4__title">МАРИНА</h3>
+								<p>«А меня на таможне не пропускали: фото на паспорте было не похоже на меня! Пришлось рассказывать таможеннице о нашей системе, она записала адрес сайта. Раньше мои сыновья, чтобы меня не огорчать, уверяли, что мама должна быть большой, теплой и уютной. А сейчас смотрят на меня с восхищением и с блеском в глазах повторяют: «Мама, какая ты стройняшка!»</p>
+							</div>
+						</div>
+						<img class="s4__icon lazyload" loading="lazy" src="#" data-src="img/s4/01.png" data-srcset="img/s4/01@2x.png 2x" alt="">
+					</div>
+					<div class="s4__row s4__row--2">
+						<div class="s4__box">
+							<div class="s4__pict">
+								<img class="s4__img lazyload" loading="lazy" src="#" data-src="img/s4/pict2.png" data-srcset="img/s4/pict2@2x.png 2x" alt="">
+							</div>
+							<div class="s4__content">
+								<h3 class="s4__title">НАТАЛЬЯ</h3>
+								<p>«Если кто-то думает, что я надрываюсь в спортзале и голодаю — тот ошибается. Я не занимаюсь спортом — некогда, а ем больше, чем раньше, когда сидела на голодных диетах, зато теперь я ем вкусно и полноценно, открыла для себя множество новых продуктов. И семья потихоньку привыкает к правильному питанию, а значит все, кто мне дорог, долие годы будут здоровыми!»</p>
+							</div>
+						</div>
+						<img class="s4__icon lazyload" loading="lazy" src="#" data-src="img/s4/02.png" data-srcset="img/s4/02@2x.png 2x" alt="">
+					</div>
+					
+					
+				</div>
+			</section>
+			<!--section 4-->
+
+			<!--section 5-->
+			<section class="s5">
+				<div class="container s5__container">
+					<h2 class="title s5__title">
+						ВАШ ПЛАН ПИТАНИЯ СОСТАВЛЕН ПО ЗАПАТЕНТОВАННОЙ
+						СИСТЕМЕ “ФОРМУЛА&nbsp;СТРОЙНОСТИ” ТАТЬЯНЫ МАЛАХОВОЙ
+					</h2>
+
+					<div class="s5__author">
+						<div class="row">
+							<div class="col-lg-6">
+								<div class="s5__pict">
+									<img class="s5__img lazyload" loading="lazy" src="#" data-src="img/s5/author.png" data-srcset="img/s5/author@2x.png 2x" alt="">
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="s5__content">
+									<ul class="list s5__list">
+										<li class="list__item">ИНЖЕНЕР-ТЕПЛОЭНЕРГЕТИК С ИННОВАЦИОННЫМ ПОДХОДОМ К ДОСТИЖЕНИЮ ИДЕАЛЬНОГО ВЕСА</li>
+										<li class="list__item">ПОМОГЛА ДЕСЯТКАМ ТЫСЯЧ ЛЮДЕЙ ПО ВСЕМУ МИРУ ПОДРУЖИТЬСЯ С ПИТАНИЕМ И ТЕЛОМ</li>
+										<li class="list__item">АВТОР УНИКАЛЬНОЙ, ЗАПАТЕНТОВАННОЙ СИСТЕМЫ, ДОСТУПНОЙ КАЖДОМУ</li>
+										<li class="list__item">ЧЛЕН «НАЦИОНАЛЬНОГО ОБЩЕСТВА ДИЕТОЛОГОВ»</li>
+										<li class="list__item">АВТОР 5 КНИГ И МНОЖЕСТВА ДРУГИХ ПОЛЕЗНЫХ МАТЕРИАЛОВ, КОТОРЫЕ УЧАТ ЛЮДЕЙ СТРОЙНЕТЬ, ПРАВИЛЬНО СОЧЕТАЯ ПРОДУКТЫ</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</section>
+			<!--section 5-->
+
+			<!--section 6-->
+			<section class="s6">
+				<div class="container s6__container">
+
+					<div class="s6__review">
+						<div class="s6__pict">
+							<img class="s6__img lazyload" loading="lazy" src="#" data-src="img/s6/01.png" data-srcset="img/s6/01@2x.png 2x" alt="">
+						</div>
+						<div class="s6__content">
+							<h3 class="s6__title">Валентина</h3>
+							<p>«С 20 лет жила с приговором врачей, что полнота - мой неизбежный спутник (из-за аутоимунного тироидита). И так к 64 годам накопила 130 килограммов, что очень сильно осложняло мою жизнь: больной позвоночник, гоноартроз коленных суставов, бронхиальная астма, гипертония и много других болячек. Но судьба подарила мне второй шанс, познакомив с системой «Будь стройной!». Я поверила и менее чем за 2 года уменьшилась ровно вдвое: до 65 кг! Забыла про астму, гипертонию. Гораздо легче стало ногам и спине. Я возвращаюсь к нормальной жизни! И в моем возрасте возможны такие чудесные превращения, при этом без всякого насилия над организмом, без физических нагрузок, которые мне пока противопоказаны. Всего только сбалансировав питание по системе «Будь стройной!»</p>
+						</div>
+					</div>
+					<div class="s6__review s6__review--reverse">
+						<div class="s6__pict">
+							<img class="s6__img lazyload" loading="lazy" src="#" data-src="img/s6/02.png" data-srcset="img/s6/02@2x.png 2x" alt="">
+						</div>
+						<div class="s6__content">
+							<h3 class="s6__title">Татьяна</h3>
+							<p>«Встретилась с системой 1 марта 2012 года, и теперь наша дружба навеки. Ничего более комфортного в своей жизни не встречала. Правду говорят: все гениальное — просто. За год — минус 41 кг, а в остальном только плюсики: здоровье стало значительно лучше, настроение отличное, активность супер, да ещё, говорят, помолодела!»</p>
+						</div>
+					</div>
+					<div class="s6__review">
+						<div class="s6__pict">
+							<img class="s6__img lazyload" loading="lazy" src="#" data-src="img/s6/03.png" data-srcset="img/s6/03@2x.png 2x" alt="">
+						</div>
+						<div class="s6__content">
+							<h3 class="s6__title">Марина</h3>
+							<p>«Сброшены не только 30 килограммов, но и куча болезней. Это самое главное для меня. Благодаря системе „Будь стройной!" я стала счастливым человеком. И таких, как я, теперь очень много. Мы поверили в себя и в систему — результат налицо. Спасибо!»</p>
+						</div>
+					</div>
+
+					<div class="btn-container">
+						<a data-fancybox data-src="#hidden-content" href="javascript:;" class="btn btn--active s6__btn">
+							<span class="btn__inner">ДА, Я ХОЧУ ПРИНЯТЬ УЧАСТИЕ</span>						
+						</a>
+					</div>	
+
+				</div>
+			</section>
+			<!--section 6-->
+
+			<!--section 7-->
+			<section class="s7">
+				<div class="container s7__container">
+
+					<h2 class="title s7__title">
+						Часто задаваемые вопросы
+					</h2>
+
+					<div class="accordion">
+						<div class="accordion__item">
+							<input type="checkbox" class="accordion__control sr-only" name="faq" id="faq1">
+							<label for="faq1" class="accordion__head">
+								Как будет проходить марафон?
+								<span class="accordion__icon"></span>
+							</label>
+							<div class="accordion__content">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit possimus voluptates et inventore dignissimos molestias sit laborum laboriosam excepturi aliquid illo quam explicabo ratione, dolorum corporis. Officiis doloribus quo hic?</p>
+							</div>
+						</div>
+						<div class="accordion__item">
+							<input type="checkbox" class="accordion__control sr-only" name="faq" id="faq2">
+							<label for="faq2" class="accordion__head">
+								Смогу ли я проходить марафон с телефона
+								<span class="accordion__icon"></span>
+							</label>
+							<div class="accordion__content">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit possimus voluptates et inventore dignissimos molestias sit laborum laboriosam excepturi aliquid illo quam explicabo ratione, dolorum corporis. Officiis doloribus quo hic?</p>
+							</div>
+						</div>
+						<div class="accordion__item">
+							<input type="checkbox" class="accordion__control sr-only" name="faq" id="faq3">
+							<label for="faq3" class="accordion__head">
+								Боюсь, что продукты будут обходиться слишком дорого
+								<span class="accordion__icon"></span>
+							</label>
+							<div class="accordion__content">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit possimus voluptates et inventore dignissimos molestias sit laborum laboriosam excepturi aliquid illo quam explicabo ratione, dolorum corporis. Officiis doloribus quo hic?</p>
+							</div>
+						</div>
+						<div class="accordion__item">
+							<input type="checkbox" class="accordion__control sr-only" name="faq" id="faq4">
+							<label for="faq4" class="accordion__head">
+								У меня не получается оплатить
+								<span class="accordion__icon"></span>
+							</label>
+							<div class="accordion__content">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit possimus voluptates et inventore dignissimos molestias sit laborum laboriosam excepturi aliquid illo quam explicabo ratione, dolorum corporis. Officiis doloribus quo hic?</p>
+							</div>
+						</div>
+						<div class="accordion__item">
+							<input type="checkbox" class="accordion__control sr-only" name="faq" id="faq5">
+							<label for="faq5" class="accordion__head">
+								Я не умею готовить
+								<span class="accordion__icon"></span>
+							</label>
+							<div class="accordion__content">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit possimus voluptates et inventore dignissimos molestias sit laborum laboriosam excepturi aliquid illo quam explicabo ratione, dolorum corporis. Officiis doloribus quo hic?</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</section>
+			<!--section 7-->
+
+		</main>
+		<!--content.end-->
+
+	</div>
+	<!--root.end-->
+
+	<!--modal1.begin-->
+	<div style="display:none;" id="hidden-content">
+		<div class="header-popup">
+			<div class="progres-bar-wrap">
+				<div class="progres-bar-descr">50% ЗАВЕРШЕНО</div>
+				<div class="lp-progressbar-react__bar lp-progressbar-animation">
+					<div class="bar-completed">
+						<div class="is-animating"></div>
+					</div>
+				</div>
+			</div>
+			<div class="left-popup-seide">
+				<img class="lazyload" loading="lazy" src="#" data-src="images/key.png" alt="">
+			</div>
+			<div class="right-popup-side">
+				<div class="title-form">
+					Займите свое место пока они еще&nbsp;есть!
+				</div>
+				<?
+				$arr = array('utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term', 'ID_VK', 'cid');
+				$add = '';
+				foreach($arr as $name){
+					$data = (!empty($_GET[$name])) ? $_GET[$name] : '';
+					$input_text = htmlspecialchars($data);
+					$add .= '&'.$name.'='.$input_text;
+				}
+				?>
+
+				<form class="one-form" id="ltForm472963"  action="https://online.stroyneyu.ru/pl/lite/block-public/process-html?id=1168922232" method="post" data-open-new-window="0">
+					<input type="hidden" name="formParams[setted_offer_id]">
+					<input name="formParams[userCustomFields][145331]" type="hidden" value="<?=$_GET['ID_VK']?>">
+					<input name="formParams[userCustomFields][405421]" type="hidden" value="<?=$_GET['utm_source']?>">
+					<input name="formParams[userCustomFields][405422]" type="hidden" value="<?=$_GET['utm_medium']?>">
+					<input name="formParams[userCustomFields][405423]" type="hidden" value="<?=$_GET['utm_campaign']?>">
+					<input name="formParams[userCustomFields][405424]" type="hidden" value="<?=$_GET['utm_content']?>">
+					<input name="formParams[userCustomFields][405425]" type="hidden" value="<?=$_GET['utm_term']?>">
+					<input name="formParams[userCustomFields][345656]" type="hidden" value="<?=$_GET['cid']?>">
+					<label>
+						<input type="text" maxlength="60" data-validation="email" placeholder="Введите свое имя "
+							name="formParams[full_name]" value="">
+					</label>
+					<label>
+						<input type="email" maxlength="60" data-validation="email" placeholder="Введите свой E-mail"
+							name="formParams[email]" value="">
+					</label>
+					<label>
+						<input type="tel" maxlength="60" data-validation="email" placeholder="Введите свой телефон"
+							name="formParams[phone]" value="">
+					</label>
+					<button type="submit" id="button754251" class="feedback"
+					onclick="if(window['btnprs61542f88480f7']){return false;}window['btnprs61542f88480f7']=true;setTimeout(function(){window['btnprs61542f88480f7']=false},6000);return true;">
+						Зарегистрироваться!
+					</button>
+					<input type="hidden" id="49257861542f8834dad" name="__gc__internal__form__helper" class="__gc__internal__form__helper" value="">
+<input type="hidden" id="49257861542f8834dadref" name="__gc__internal__form__helper_ref" class="__gc__internal__form__helper_ref" value="">
+<input type="hidden" name="requestTime" value="1632907144">
+<input type="hidden" name="requestSimpleSign" value="a815de9d61b9e287c9a458f66c4f3797">
+<input type="hidden" name="isHtmlWidget" value="1"/>
+				</form>
+				<script>
+					window.addEventListener('load', function () {
+						let loc = document.getElementById("49257861542f8834dad");
+		loc.value = window.location.href;
+		let ref = document.getElementById("49257861542f8834dadref");
+		ref.value = document.referrer;
+		
+						let statUrl = "https://online.kizos.ru/stat/counter?ref=" + encodeURIComponent(document.referrer) +
+							"&loc=" + encodeURIComponent(document.location.href);
+						document.getElementById('gccounterImgContainer').innerHTML =
+							"<img width=1 height=1 style='display:none' id='gccounterImg' src='" + statUrl + "'/>";
+					});
+				</script>
+				<div class="privacy-police">
+					Отправляя форму, вы&nbsp;соглашаетесь с&nbsp;действующей <a
+						href="https://shkolaketo.ru/docs/privacy.html" target="_blank">политикой&nbsp;приватности</a>.
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+	<!--modal1.end-->
+
+	<!--script.begin-->
+	<script async src="vendor/lazysizes/lazysizes.min.js"></script>
+	<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script defer src="vendor/fancybox/jquery.fancybox.min.js"></script>
+	<script defer src="js/main.js?v=1"></script>
+	<!--script.end-->
+
+</body>
+
+</html>
